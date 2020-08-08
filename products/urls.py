e793493a3a4186441e4fff41392af8544fc5445c
Hemanth <hemanth.hku@gmail.com>
2020-08-08 20:04:07 +0530

    Cotegories

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,8 @@ app_name = 'products'
 urlpatterns = [
     path('', views.index, name='index'),
     path('product/', views.product, name='product'),
+    path('laptops/', views.laptops, name='laptops'),
+    path('mobiles/', views.mobiles, name='mobiles'),
+    path('sensors/', views.sensors, name='sensors'),
+    path('electronic_components/', views.electronic_components, name='electronic_components'),
 ]
--- products/views.py
+++ products/views.py
@@ -6,8 +6,28 @@ import math
 def index(request):
     return render(request, "index.html")
 
+def laptops(request):
+    laptops = Product.objects.filter(category_id=2)
+    context = {'laptops':laptops}
+    return render(request, "products/categories.html", context)
+
+def mobiles(request):
+    mobiles = Product.objects.filter(category_id=1)
+    context = {'mobiles':mobiles}
+    return render(request, "products/categories.html", context)
+
+def sensors(request):
+    sensors = Product.objects.filter(category_id=3)
+    context = {'sensors':sensors}
+    return render(request, "products/categories.html", context)
+
+def electronic_components(request):
+    electronic_components = Product.objects.filter(category_id=4)
+    context = {'electronic_components':laptops}
+    return render(request, "products/categories.html", context)
+
 def product(request):
-    product = Product.objects.get(id=8)
+    product = Product.objects.get(id=10)
     rating = list(range(product.rating))
     rating_un = list(range(5-len(rating)))
     save = ((product.mrp-product.deal_price)/product.mrp)*100
