2508000a7cae2bb4605d4122807cd559c03f103e
Hemanth <hemanth.hku@gmail.com>
2020-08-16 14:21:27 +0530

    settings.py for deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -83,13 +83,13 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecommerce',
-        'USER': 'postgres',
-        'PASSWORD': 'hemanth.97',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'ecommerce',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'hemanth.97',
+        # 'HOST': 'localhost'
     }
 }
 
