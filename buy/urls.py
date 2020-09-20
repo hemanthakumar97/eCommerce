f0170470e928607b473063d8f009280cbcc9b3eb
Hemanth <hemanth.hku@gmail.com>
2020-09-20 21:30:52 +0530

    buy

--- buy/urls.py
+++ buy/urls.py
@@ -6,4 +6,6 @@ app_name= 'buy'
 urlpatterns = [
     path("payment/<int:id>/", payment, name='payment'),
     path("address/", address, name='address'),
+    path("place_order/", place_order, name='place_order'),
+    path("confirm_page/", confirm_page, name='confirm_page'),
 ]
--- buy/views.py
+++ buy/views.py
@@ -19,5 +19,16 @@ def address(request):
         id = request.POST['address']
         obj = Address.objects.get(id=id)
         Order.objects.update(user=user, address=obj)
+        return redirect("buy:place_order")
     addresses = Address.objects.filter(user=user)
     return render(request, "buy/address.html",{"addresses":addresses})
+
+def place_order(request):
+    if request.method=="POST":
+        return redirect("buy:confirm_page")
+    address = Order.objects.get(id=5).address
+    return render(request, "buy/place_order.html",{"address":address})
+
+def confirm_page(request):
+    address = Order.objects.get(id=5).address
+    return render(request,"buy/confirm_page.html",{"address":address})
